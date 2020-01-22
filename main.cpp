edb4abc78fa785261d0c0af8d1cbde20a2779f34
mohammad amin <m.a.geramimehr.0000@gmail.com>
2020-01-22 20:18:46 +0330

    yes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
-
+#include "writeboard.h"
+using namespace std;
 int main() {
-    std::cout << "Hell, World!" << std::endl;
-    return 0;
+writeboard();
+	
+	
+	
+	
+	return 0;
 }
