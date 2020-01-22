d5a8f039217a21daf1c68085777e3d8ff4532602
ali samiinia <asameeinia@gmail.com>
2020-01-22 21:13:30 +0330

    a

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
+#include <stdio.h>
 #include <iostream>
+#include "chess.h"
 #include "writeboard.h"
-using namespace std;
 int main() {
-writeboard();
-	
-	
-	
-	
+    char a;
+    int b;
+    char c;
+    int d;
+    while (!mat()) {
+        writeboard()
+        cin >> a >> b;
+        if (!canmove(a, b)) {
+            cout << "this ... cannot move\nchose something else" << a << "\n" << b;
+            cin >> a >> b;
+        }
+        cin >> c >> d;
+        if (!cango(c, d)) {
+            cout << "chose somewhere else";
+            cin >> c >> d;
+        }
+        move(a, b, c, d);
+    }
+    cout << "mat!";
 	return 0;
 }
